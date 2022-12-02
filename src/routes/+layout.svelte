0e93cbc059dc5a23bc96056a13f992331db2f6c3
unknown <suvro.ghosh.contact@gmail.com>
2022-12-02 13:21:30 +0400

    initial

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import PageHeader from "../lib/PageHeader.svelte";
     import Navigation from "../lib/Navigation.svelte";
     import PageFooter from "../lib/PageFooter.svelte";
-    let PageHeaderInfo=["Premium UAE ERP solutions","+971 2 555 8686","+971 50 211 3555","sales@shertatechservices.com"]
+    export let PageHeaderInfo;
 
 </script>
 
