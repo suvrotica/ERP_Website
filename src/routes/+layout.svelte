bc6e6916428043836fae8f1d150389364a08f5a3
unknown <suvro.ghosh.contact@gmail.com>
2022-12-14 20:16:43 +0400

    initial

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,14 @@
     import Navigation from "../lib/Navigation.svelte";
     import PageFooter from "../lib/PageFooter.svelte";
     import Analytics from "../lib/analytics.svelte";
+    import Meta from "../lib/meta.svelte";
     export let PageHeaderInfo;
 
 </script>
 
 <PageHeader {PageHeaderInfo}/>
-<Navigation />
-<Analytics />
+<Navigation/>
+<Meta/>
+<Analytics/>
 <slot></slot>
 <PageFooter/>
