5ed4f7c11d923b2fc5f4b50eebe9c5a68a1a11d6
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-04-08 17:43:17 +0530

    34

--- src/lib/PageFooter.svelte
+++ src/lib/PageFooter.svelte
@@ -1,5 +1,20 @@
 <script>
     import DownloadPdfButton from './brochureDownload.svelte';
+  
+    /**
+   * @type {string}
+   */
+    let currentMonthYear;
+  
+    $: {
+      const currentDate = new Date();
+      const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+      const currentMonth = monthNames[currentDate.getMonth()];
+      const currentYear = currentDate.getFullYear();
+  
+      currentMonthYear = `${currentMonth} ${currentYear}`;
+    }
   </script>
   
   
@@ -11,7 +26,7 @@
         <div class="container-xxl">
             
             <DownloadPdfButton />
-            <span class="small "><span><i class="bi bi-c-square-fill"></i></span>     Shertatech Copyright 2022 <i class="bi bi-file-earmark-lock2-fill"></i> </span>
+            <span class="small "><span><i class="bi bi-c-square-fill"></i></span>     Copyright {currentMonthYear} <i class="bi bi-file-earmark-lock2-fill"></i> </span>
             
             <a href="https://wa.me/+971502113555" target="_blank" rel="noreferrer"class="btn btn-sm btn-success text-light" ><i class="bi bi-whatsapp"></i> Whatsapp</a>
             
