115d2cd12fed9091cec655df08124eaba0a4ffcb
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-04-08 17:32:54 +0530

    33

---
+++ src/lib/brochureDownload.svelte
@@ -0,0 +1,6 @@
+<script>
+    let pdfFileUrl = './erp.PDF';
+  </script>
+  <a href={pdfFileUrl} download class="btn btn-sm btn-primary text-light" ><i class="bi bi-file-earmark-arrow-down-fill"></i>Brochure</a>
+  
+ 
